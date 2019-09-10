1c043642b7ed34f585d2782c93a39ae2d28c9b2b
Tue Boesen <tue.boesen@gmail.com>
2019-09-10 09:15:36 -0700

    Updated to new pytorch version

--- pytorch/main.py
+++ pytorch/main.py
@@ -4,7 +4,6 @@
 # 4.0 International License. To view a copy of this license, visit
 # http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
 # Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-
 import re
 import argparse
 import os
@@ -196,7 +195,7 @@ def update_ema_variables(model, ema_model, alpha, global_step):
 def train(train_loader, model, ema_model, optimizer, epoch, log):
     global global_step
 
-    class_criterion = nn.CrossEntropyLoss(size_average=False, ignore_index=NO_LABEL).cuda()
+    class_criterion = nn.CrossEntropyLoss(reduction='sum', ignore_index=NO_LABEL).cuda()
     if args.consistency_type == 'mse':
         consistency_criterion = losses.softmax_mse_loss
     elif args.consistency_type == 'kl':
@@ -220,11 +219,12 @@ def train(train_loader, model, ema_model, optimizer, epoch, log):
         meters.update('lr', optimizer.param_groups[0]['lr'])
 
         input_var = torch.autograd.Variable(input)
-        ema_input_var = torch.autograd.Variable(ema_input, volatile=True)
-        target_var = torch.autograd.Variable(target.cuda(async=True))
+        with torch.no_grad():
+            ema_input_var = torch.autograd.Variable(ema_input)
+        target_var = torch.autograd.Variable(target.cuda(non_blocking=True))
 
         minibatch_size = len(target_var)
-        labeled_minibatch_size = target_var.data.ne(NO_LABEL).sum()
+        labeled_minibatch_size = target_var.data.ne(NO_LABEL).sum().item()
         assert labeled_minibatch_size > 0
         meters.update('labeled_minibatch_size', labeled_minibatch_size)
 
@@ -246,41 +246,41 @@ def train(train_loader, model, ema_model, optimizer, epoch, log):
         if args.logit_distance_cost >= 0:
             class_logit, cons_logit = logit1, logit2
             res_loss = args.logit_distance_cost * residual_logit_criterion(class_logit, cons_logit) / minibatch_size
-            meters.update('res_loss', res_loss.data[0])
+            meters.update('res_loss', res_loss.item())
         else:
             class_logit, cons_logit = logit1, logit1
             res_loss = 0
 
         class_loss = class_criterion(class_logit, target_var) / minibatch_size
-        meters.update('class_loss', class_loss.data[0])
+        meters.update('class_loss', class_loss.item())
 
         ema_class_loss = class_criterion(ema_logit, target_var) / minibatch_size
-        meters.update('ema_class_loss', ema_class_loss.data[0])
+        meters.update('ema_class_loss', ema_class_loss.item())
 
         if args.consistency:
             consistency_weight = get_current_consistency_weight(epoch)
             meters.update('cons_weight', consistency_weight)
             consistency_loss = consistency_weight * consistency_criterion(cons_logit, ema_logit) / minibatch_size
-            meters.update('cons_loss', consistency_loss.data[0])
+            meters.update('cons_loss', consistency_loss.item())
         else:
             consistency_loss = 0
             meters.update('cons_loss', 0)
 
         loss = class_loss + consistency_loss + res_loss
-        assert not (np.isnan(loss.data[0]) or loss.data[0] > 1e5), 'Loss explosion: {}'.format(loss.data[0])
-        meters.update('loss', loss.data[0])
+        assert not (np.isnan(loss.item()) or loss.item() > 1e5), 'Loss explosion: {}'.format(loss.item())
+        meters.update('loss', loss.item())
 
         prec1, prec5 = accuracy(class_logit.data, target_var.data, topk=(1, 5))
-        meters.update('top1', prec1[0], labeled_minibatch_size)
-        meters.update('error1', 100. - prec1[0], labeled_minibatch_size)
-        meters.update('top5', prec5[0], labeled_minibatch_size)
-        meters.update('error5', 100. - prec5[0], labeled_minibatch_size)
+        meters.update('top1', prec1, labeled_minibatch_size)
+        meters.update('error1', 100. - prec1, labeled_minibatch_size)
+        meters.update('top5', prec5, labeled_minibatch_size)
+        meters.update('error5', 100. - prec5, labeled_minibatch_size)
 
         ema_prec1, ema_prec5 = accuracy(ema_logit.data, target_var.data, topk=(1, 5))
-        meters.update('ema_top1', ema_prec1[0], labeled_minibatch_size)
-        meters.update('ema_error1', 100. - ema_prec1[0], labeled_minibatch_size)
-        meters.update('ema_top5', ema_prec5[0], labeled_minibatch_size)
-        meters.update('ema_error5', 100. - ema_prec5[0], labeled_minibatch_size)
+        meters.update('ema_top1', ema_prec1, labeled_minibatch_size)
+        meters.update('ema_error1', 100. - ema_prec1, labeled_minibatch_size)
+        meters.update('ema_top5', ema_prec5, labeled_minibatch_size)
+        meters.update('ema_error5', 100. - ema_prec5, labeled_minibatch_size)
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
@@ -312,53 +312,54 @@ def train(train_loader, model, ema_model, optimizer, epoch, log):
 
 
 def validate(eval_loader, model, log, global_step, epoch):
-    class_criterion = nn.CrossEntropyLoss(size_average=False, ignore_index=NO_LABEL).cuda()
+    class_criterion = nn.CrossEntropyLoss(reduction='sum', ignore_index=NO_LABEL).cuda()
     meters = AverageMeterSet()
 
     # switch to evaluate mode
     model.eval()
 
     end = time.time()
-    for i, (input, target) in enumerate(eval_loader):
-        meters.update('data_time', time.time() - end)
+    with torch.no_grad():
+        for i, (input, target) in enumerate(eval_loader):
+            meters.update('data_time', time.time() - end)
 
-        input_var = torch.autograd.Variable(input, volatile=True)
-        target_var = torch.autograd.Variable(target.cuda(async=True), volatile=True)
+            input_var = torch.autograd.Variable(input)
+            target_var = torch.autograd.Variable(target.cuda(non_blocking=True))
 
-        minibatch_size = len(target_var)
-        labeled_minibatch_size = target_var.data.ne(NO_LABEL).sum()
-        assert labeled_minibatch_size > 0
-        meters.update('labeled_minibatch_size', labeled_minibatch_size)
+            minibatch_size = len(target_var)
+            labeled_minibatch_size = target_var.data.ne(NO_LABEL).sum().item()
+            assert labeled_minibatch_size > 0
+            meters.update('labeled_minibatch_size', labeled_minibatch_size)
 
-        # compute output
-        output1, output2 = model(input_var)
-        softmax1, softmax2 = F.softmax(output1, dim=1), F.softmax(output2, dim=1)
-        class_loss = class_criterion(output1, target_var) / minibatch_size
+            # compute output
+            output1, output2 = model(input_var)
+            softmax1, softmax2 = F.softmax(output1, dim=1), F.softmax(output2, dim=1)
+            class_loss = class_criterion(output1, target_var) / minibatch_size
 
-        # measure accuracy and record loss
-        prec1, prec5 = accuracy(output1.data, target_var.data, topk=(1, 5))
-        meters.update('class_loss', class_loss.data[0], labeled_minibatch_size)
-        meters.update('top1', prec1[0], labeled_minibatch_size)
-        meters.update('error1', 100.0 - prec1[0], labeled_minibatch_size)
-        meters.update('top5', prec5[0], labeled_minibatch_size)
-        meters.update('error5', 100.0 - prec5[0], labeled_minibatch_size)
+            # measure accuracy and record loss
+            prec1, prec5 = accuracy(output1.data, target_var.data, topk=(1, 5))
+            meters.update('class_loss', class_loss.item(), labeled_minibatch_size)
+            meters.update('top1', prec1, labeled_minibatch_size)
+            meters.update('error1', 100.0 - prec1, labeled_minibatch_size)
+            meters.update('top5', prec5, labeled_minibatch_size)
+            meters.update('error5', 100.0 - prec5, labeled_minibatch_size)
 
-        # measure elapsed time
-        meters.update('batch_time', time.time() - end)
-        end = time.time()
+            # measure elapsed time
+            meters.update('batch_time', time.time() - end)
+            end = time.time()
 
-        if i % args.print_freq == 0:
-            LOG.info(
-                'Test: [{0}/{1}]\t'
-                'Time {meters[batch_time]:.3f}\t'
-                'Data {meters[data_time]:.3f}\t'
-                'Class {meters[class_loss]:.4f}\t'
-                'Prec@1 {meters[top1]:.3f}\t'
-                'Prec@5 {meters[top5]:.3f}'.format(
-                    i, len(eval_loader), meters=meters))
+            if i % args.print_freq == 0:
+                LOG.info(
+                    'Test: [{0}/{1}]\t'
+                    'Time {meters[batch_time]:.3f}\t'
+                    'Data {meters[data_time]:.3f}\t'
+                    'Class {meters[class_loss]:.4f}\t'
+                    'Prec@1 {meters[top1]:.3f}\t'
+                    'Prec@5 {meters[top5]:.3f}'.format(
+                        i, len(eval_loader), meters=meters))
 
     LOG.info(' * Prec@1 {top1.avg:.3f}\tPrec@5 {top5.avg:.3f}'
-          .format(top1=meters['top1'], top5=meters['top5']))
+        .format(top1=meters['top1'], top5=meters['top5']))
     log.record(epoch, {
         'step': global_step,
         **meters.values(),
@@ -413,7 +414,7 @@ def accuracy(output, target, topk=(1,)):
     res = []
     for k in topk:
         correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-        res.append(correct_k.mul_(100.0 / labeled_minibatch_size))
+        res.append(correct_k.mul_(100.0 / labeled_minibatch_size.float()).item())
     return res
 
 
